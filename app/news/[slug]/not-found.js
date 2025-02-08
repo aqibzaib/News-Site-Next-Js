73717c4acf4d7896282fdc969e4a710760697e27
Aqib <chaqib4u@gmail.com>
2025-02-08 17:36:36 +0500

    first commit

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewsNotFoundPage() {
+  return (
+    <div id="root">
+      <h1>Not Found!</h1>
+      <p>Unfortunately we could not found the requested article.</p>
+    </div>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFoundPage() {
+  return (
+    <div id="root">
+      <h1>Not Found!</h1>
+      <p>The request source could not be Found!</p>
+    </div>
+  );
+}
