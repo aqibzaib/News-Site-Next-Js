2a3ca74f57348f00c1f77e467eef3c951e052c99
Aqib <chaqib4u@gmail.com>
2025-02-08 19:08:20 +0500

    add filter methond in archive new year

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,5 +1,20 @@
-import React from "react";
+import Link from "next/link";
+import { getAvailableNewsYears } from "@/lib/news";
 
 export default function ArchivePage() {
-  return <div>ArchiveNewsPage</div>;
+  const links = getAvailableNewsYears(); // Fixed function call
+
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
